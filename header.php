206c227ecc3a4fc9dc6c8cf0de439a8c1693e690
shivek-brightcode <shivek.sahay@brightcodess.com>
2019-02-13 15:28:25 +0530

    first intiall commmit

---
+++ header.php
@@ -0,0 +1,49 @@
+<!--==========================
+  Header
+  ============================-->
+  <header id="header" class="fixed-top">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-2 d-none d-xl-block">&nbsp;&nbsp;</div>
+        <div class="logo float-left col-lg-3">
+          <!-- Uncomment below if you prefer to use an image logo -->
+          <!-- <h1 class="text-light"><a href="#header"><span>NewBiz</span></a></h1> -->
+          <a href="#intro" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a>
+        </div>
+
+        <nav class="col-lg-6 main-nav float-right d-none d-xl-block">
+          <ul>
+            <li class="active"><a href="#intro">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#team">Team</a></li>
+            <!--<li class="drop-down"><a href="">Drop Down</a>
+              <ul>
+                <li><a href="#">Drop Down 1</a></li>
+                <li class="drop-down"><a href="#">Drop Down 2</a>
+                  <ul>
+                    <li><a href="#">Deep Drop Down 1</a></li>
+                    <li><a href="#">Deep Drop Down 2</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Drop Down 3</a></li>
+                <li><a href="#">Drop Down 4</a></li>
+                <li><a href="#">Drop Down 5</a></li>
+              </ul>
+            </li>-->
+            <li><a href="#contact">Contact Us</a></li>
+            <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+            <li>
+              <form>
+              <div class="col-12">
+                <input type="text" name="search" class="search_box form-control" placeholder="Search Here"/>
+              </div>
+              </form>
+            </li>
+          </ul>
+        </nav><!-- .main-nav -->
+        <div class="col-lg-1"></div>
+      </div>
+    </div>
+  </header><!-- #header -->
